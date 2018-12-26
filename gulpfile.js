d4a1d6cede31b4ff38ea3adabd32b9122df111dd
ahmdsyauqi <ahmadsyauqi.stmik2014@gmail.com>
2018-12-26 14:58:45 +0800

    config gulp config for concat fonts

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,45 @@
-var gulp				  = require('gulp'),
-    sass 				  = require('gulp-sass'),
-    autoprefixer  = require('gulp-autoprefixer'),
-    flatten       = require('gulp-flatten'),
-    browserSync   = require('browser-sync'),
-    webpack       = require('webpack-stream'),
-    gulpif        = require('gulp-if'),
-    extend        = require('extend'),
-    buildType     = "dev"
+var gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  autoprefixer = require('gulp-autoprefixer'),
+  flatten = require('gulp-flatten'),
+  browserSync = require('browser-sync'),
+  webpack = require('webpack-stream'),
+  gulpif = require('gulp-if'),
+  extend = require('extend'),
+  buildType = "dev"
 
 // sass compiler
-gulp.task('sass', function() {
+gulp.task('sass', function () {
   console.log(buildType);
   return gulp.src("dist/sass/**/*.scss")
-  .pipe(sass({
-    outputStyle: gulpif(buildType=="prod", 'compressed', 'compact'),
-    includePaths: ['node_modules/']
-  }))
-  .pipe(autoprefixer())
-  .pipe(gulp.dest("app/css"));
+    .pipe(sass({
+      outputStyle: gulpif(buildType == "prod", 'compressed', 'compact'),
+      includePaths: ['node_modules/']
+    }))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest("app/css"));
 });
 
 // icon task
-gulp.task('fonts', function() {
-  return gulp.src('./node_modules/**/*.{eot,svg,ttf,woff,woff2}')
-  .pipe(flatten())
-  .pipe(gulp.dest("app/fonts"));
+gulp.task('fonts', function () {
+  return gulp.src([
+    './node_modules/**/*.{eot,svg,ttf,woff,woff2}', './dist/sass/fonts/**/*.{eot,svg,ttf,woff,woff2}'
+  ])
+    .pipe(flatten())
+    .pipe(gulp.dest("app/fonts"));
 });
 
 // js compiler
-gulp.task('js', function(){
+gulp.task('js', function () {
   var config = extend({}, require('./webpack.config.js'), {
-    mode: gulpif(buildType=="prod", 'production', 'development'),
+    mode: gulpif(buildType == "prod", 'production', 'development'),
   });
   return gulp.src("dist/js/app.js")
-  .pipe(webpack(config))
-  .pipe(gulp.dest("app/js/"));
+    .pipe(webpack(config))
+    .pipe(gulp.dest("app/js/"));
 });
 
-gulp.task('watch', ['sass', 'fonts', 'js'], function() {
+gulp.task('watch', ['sass', 'fonts', 'js'], function () {
   browserSync.init({
     server: {
       baseDir: "./app/"
@@ -51,12 +53,12 @@ gulp.task('watch', ['sass', 'fonts', 'js'], function() {
 });
 
 // serve task
-gulp.task('dev', function() {
+gulp.task('dev', function () {
   gulp.start(['sass', 'fonts', 'js'])
 });
 
 //prod taks
-gulp.task('prod', function(){
+gulp.task('prod', function () {
   buildType = 'prod'
   gulp.start(['sass', 'fonts', 'js'])
 })
