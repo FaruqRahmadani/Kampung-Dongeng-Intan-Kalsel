532e0c708f600855a700c20edd8475f0ad998a4d
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-12-29 00:03:45 +0800

    update navbar resv

--- dist/js/_src/navbar.js
+++ dist/js/_src/navbar.js
@@ -11,19 +11,23 @@ $('.nav-link').on('click', function (e) {
 })
 
 
-$('.navbar-toggler').click(function () {
-  if ($("nav").height() != 100) {
-    $("nav").animate({ height: "100%" })
-    $('.navbar-toggler').css('transform', 'rotateY(180deg)')
-    setTimeout(function () {
-      $('span', '.navbar-toggler').addClass('fa-times').removeClass('fa-bars')
-    }, 250);
-  }
-  if ($("nav").height() != 0) {
-    $("nav").animate({ height: "0%" })
-    $('.navbar-toggler').css('transform', 'rotateY(0deg)')
-    setTimeout(function () {
-      $('span', '.navbar-toggler').addClass('fa-bars').removeClass('fa-times')
-    }, 250);
-  }
+$('button.navbar-toggler').click(function () {
+  if ($("nav").height() != 100) openNav()
+  if ($("nav").height() != 0) closeNav()
 })
+
+function openNav(){
+  $("nav").animate({ height: "100%" })
+  $('.navbar-toggler').css('transform', 'rotateY(180deg)')
+  setTimeout(function () {
+    $('span', '.navbar-toggler').addClass('fa-times').removeClass('fa-bars')
+  }, 250);
+}
+
+function closeNav(){
+  $("nav").animate({ height: "0%" })
+  $('.navbar-toggler').css('transform', 'rotateY(0deg)')
+  setTimeout(function () {
+    $('span', '.navbar-toggler').addClass('fa-bars').removeClass('fa-times')
+  }, 250);
+}
